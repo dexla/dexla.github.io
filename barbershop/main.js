52997b304a1f6e8dbe6c6861c33b197a954fb0f4
Maxim Sedykin <dexla3009@gmail.com>
2018-12-30 02:21:11 +0100

    1

--- barbershop/main.js
+++ barbershop/main.js
@@ -12,31 +12,39 @@ var storage = localStorage.getItem("login");
 link.addEventListener("click", function(evt) {
     evt.preventDefault();
     popap.classList.add("modal-show");
-    console.log("клик по ссылке");
-    login.focus();
     if (storage) {
         login.value = storage;
+        password.focus ()
+    } else {
+        login.focus();
     }
 });
 close.addEventListener("click", function(evt) {
     evt.preventDefault();
     popap.classList.remove("modal-show");
-    console.log("клик по lshrt");
+    popap.classList.remove("modal-error");
 });
 map.addEventListener("click", function(evt) {
     evt.preventDefault();
     popap2.classList.add("modal-show");
-    console.log("dd");
 });
 close2.addEventListener("click", function(evt) {
     evt.preventDefault();
     popap2.classList.remove("modal-show");
-    console.log("клик по lshrt");
 });
 form.addEventListener("submit", function(evt) {
     if (!login.value || !password.value) {
         evt.preventDefault();
+        popap.classList.add("modal-error")
     } else {
         localStorage.setItem("login", login.value);
     }
 });
+window.addEventListener("keydown", function(evt) {
+    if (evt.code ===27) {
+        evt.preventDefault();
+        if (popap.classList.contains("modal-show")) {
+            popap.classList.remove("modal-show");
+        }
+    }
+})
